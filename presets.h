0c16811923331c4fec6471685e2c9f3f18f6de4b
Cellie <CelliesProjects@users.noreply.github.com>
2021-12-17 10:45:13 +0100

    Fixed '538 Dance Department' link again.

--- presets.h
+++ presets.h
@@ -13,7 +13,7 @@ const source preset[] {
     {"NPO 3FM Alternative", "http://icecast.omroep.nl/3fm-alternative-mp3"},
     {"NPO Radio4", "http://icecast.omroep.nl/radio4-bb-mp3"},
     {"NPO Radio5", "http://icecast.omroep.nl/radio5-bb-mp3"},
-    {"538 Dance Department", "http://22193.live.streamtheworld.com/TLPSTR01.mp3"},
+    {"538 Dance Department", "https://playerservices.streamtheworld.com/api/livestream-redirect/TLPSTR01.mp3"},
     {"Absoluut FM", "http://absoluutfm.stream.laut.fm/absoluutfm"},
     {"Amsterdam Funk Channel", "http://stream.afc.fm:8504/stream"},
     {"Radio 10 Disco Classics", "http://19993.live.streamtheworld.com/RADIO10.mp3"},
