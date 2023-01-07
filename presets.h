647fa7df3e5bad1e40b13218992f913c78d419b1
Cellie <CelliesProjects@users.noreply.github.com>
2023-01-07 10:49:30 +0100

    Fixed some urls

--- presets.h
+++ presets.h
@@ -8,9 +8,9 @@ struct source {
 const source preset[]{
     { "NPO Radio1", "http://icecast.omroep.nl/radio1-bb-aac" },
     { "NPO Radio2", "http://icecast.omroep.nl/radio2-bb-aac" },
-    { "NPO Radio2 Soul&Jazz", "http://icecast.omroep.nl/radio6-bb-aac" },
+    { "NPO Soul&Jazz", "http://icecast.omroep.nl/radio6-bb-aac" },
     { "NPO 3FM", "http://icecast.omroep.nl/3fm-bb-aac" },
-    { "NPO Radio4", "http://icecast.omroep.nl/radio4-bb-aac" },
+    { "NPO Klassiek", "http://icecast.omroep.nl/radio4-bb-aac" },
     { "NPO Radio5", "http://icecast.omroep.nl/radio5-bb-aac" },
     { "538 Dance Department", "http://playerservices.streamtheworld.com/api/livestream-redirect/TLPSTR01.mp3" },
     { "Absoluut FM", "http://absoluutfm.stream.laut.fm/absoluutfm" },
